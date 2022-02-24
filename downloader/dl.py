a142fc4a1006f827fc0e881eddc0a0a68512f201
Debiprasad Das <debiprasaddaskdv@gmail.com>
2022-02-24 14:10:30 +0530

    trigger enter as y(yes) rather than quitting.

--- downloader/dl.py
+++ downloader/dl.py
@@ -7,10 +7,12 @@ console = Console()
 
 def download(download_link):
     dl_now = input("Do You want to download now! y/n : ")
-    if dl_now =='y':
+   
+    if dl_now =='n':
+        console.print("OK Bye!", style = "italic violet" )
+    else:
         console.print("Downloading🙂...Please Wait...", style = "bold blue")
         r = requests.get(download_link)
         open(file_id +'.mp4', 'wb').write(r.content)
-        console.print("Download is Completed\n Exiting...", style = "italic #FFC0CB")
-    else:
-        console.print("OK Bye!", style = "italic violet" )
+        console.print("Download is Completed\nExiting...", style = "bold green")
+        
